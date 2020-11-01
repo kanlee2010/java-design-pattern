d274f898a89ec9a669f6664c57ea1d738e04481e
kanlee2010 <kanlee2010@gmail.com>
2020-11-02 00:05:41 +0900

    java-design-pattern : the first commit
    Signed-off-by: kanlee2010 <kanlee2010@gmail.com>

---
+++ strategy/GameCharacter.java
@@ -0,0 +1,17 @@
+package strategy;
+
+public class GameCharacter {
+    private Weapon weapon;
+
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
+    public void attack() {
+        if(weapon == null) {
+            System.out.println("맨손공격");
+            return;
+        }
+        weapon.attack();
+    }
+}
---
+++ strategy/Sword.java
@@ -0,0 +1,7 @@
+package strategy;
+
+public class Sword implements Weapon {
+    public void attack() {
+        System.out.println("°Ë °ø°Ý");
+    }
+}
